4e580023e3ca8e8bc9dccbc32bb055515d1f1d4b
Thomas Roselyn <tgroselyn@gmail.com>
2019-06-12 16:21:43 -0500

    saga functions written

--- src/redux/sagas/ownerSaga.js
+++ src/redux/sagas/ownerSaga.js
@@ -1,16 +1,37 @@
-import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
 
 function* addOwner(action) {
     try {
-        // yield axios.post('/api/friend', action.payload);
-        // yield put({ type: 'GET_FRIENDS' });
+        yield axios.post('post.owner.php', action.payload);
+        yield put({ type: 'FETCH_OWNERS' });
     } catch (err) {
-        console.log('addFriend saga error:', err);
+        console.log('addOwner saga error:', err);
     }
-} //end addFriend
+} //end addOwner
+
+function* fetchOwners(action) {
+    try {
+        let ownerList = yield axios.get('/get.owner.php');
+        yield put({ type: 'SET_OWNERS', payload: ownerList.data });
+    } catch (err) {
+        console.log('fetchOwners saga error:', err);
+    }
+} //end fetchOwners
+
+function* removeOwner(action) {
+    try {
+        yield axios.delete('/delete.owner.php', action.payload);
+        yield put({ type: 'FETCH_OWNERS' });
+    } catch (err) {
+        console.log('removeOwner saga error:', err);
+    }
+} //end removeOwner
 
 function* ownerSaga() {
     yield takeEvery('ADD_OWNER', addOwner);
+    yield takeEvery('FETCH_OWNERS', fetchOwners);
+    yield takeEvery('REMOVE_OWNER', removeOwner);
 }
 
 export default ownerSaga;
--- src/redux/sagas/petSaga.js
+++ src/redux/sagas/petSaga.js
@@ -1,16 +1,57 @@
-import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
 
 function* addPet(action) {
     try {
-        // yield axios.post('/api/friend', action.payload);
-        // yield put({ type: 'GET_FRIENDS' });
+        yield axios.post('post.pet.php', action.payload);
+        yield put({ type: 'FETCH_PETS' });
     } catch (err) {
-        console.log('addFriend saga error:', err);
+        console.log('addPet saga error:', err);
     }
-} //end addFriend
+} //end addPet
+
+function* fetchPets(action) {
+    try {
+        let petResponse = yield axios.get('get.pet.php');
+        yield put({ type: 'SET_PETS', payload: petResponse.data });
+    } catch (err) {
+        console.log('fetchPets saga error:', err);
+    }
+} //end fetchPets
+
+function* removePet(action) {
+    try {
+        yield axios.delete('delete.pet.php', action.payload);
+        yield put({ type: 'FETCH_PETS' });
+    } catch (err) {
+        console.log('removePet saga error:', err);
+    }
+} //end removePet
+
+function* checkInPet(action) {
+    try {
+        yield axios.put('put.checkin.php', action.payload);
+        yield put({ type: 'FETCH_PETS' });
+    } catch (err) {
+        console.log('checkInPet saga error:', err);
+    }
+} //end checkInPet
+
+function* checkOutPet(action) {
+    try {
+        yield axios.put('put.checkout.php', action.payload);
+        yield put({ type: 'FETCH_PETS' });
+    } catch (err) {
+        console.log('checkOutPet saga error:', err);
+    }
+} //end checkOutPet
 
 function* petSaga() {
     yield takeEvery('ADD_PET', addPet);
+    yield takeEvery('FETCH_PETS', fetchPets);
+    yield takeEvery('REMOVE_PET', removePet);
+    yield takeEvery('CHECK_IN_PET', checkInPet);
+    yield takeEvery('CHECK_OUT_PET', checkOutPet);
 }
 
 export default petSaga;
