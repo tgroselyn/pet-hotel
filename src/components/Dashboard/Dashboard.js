1555be749dbfc165ec0dfe2f9bbf5c55c3b99900
Thomas Roselyn <tgroselyn@gmail.com>
2019-06-12 14:32:17 -0500

    route links displaying component content

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <p>Hello from Dashboard</p>
+        )
+    }
+}
+
+export default Dashboard;
---
+++ src/components/ManageOwners/ManageOwners.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ManageOwners extends Component {
+    render() {
+        return (
+            <p>Hello from ManageOwners</p>
+        )
+    }
+}
+
+export default ManageOwners;
